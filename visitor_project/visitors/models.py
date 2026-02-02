e12afdb03eb78a1e39f0a98dbd4bf4c4a32b32b8
Hridhya Das <hridyadas02@gmail.com>
2026-02-02 07:16:31 +0530

    models

--- visitor_project/visitors/models.py
+++ visitor_project/visitors/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Visitor(models.Model):
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=15)
-    # purpose = models.CharField(max_length=200)
     purpose = models.TextField()
     check_in_time = models.DateTimeField(auto_now_add=True)
     check_out_time = models.DateTimeField(null=True, blank=True)
